fecac15d5ce8c006da1b72c1799540b1d9a207a5
aky99 <70799940+alexandru-nechifor@users.noreply.github.com>
2022-07-06 12:59:05 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
 
-const API_URL = 'http://www.omdbapi.com?apikey=4cf7c0b1';
+const API_URL = 'https://www.omdbapi.com?apikey=4cf7c0b1';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
